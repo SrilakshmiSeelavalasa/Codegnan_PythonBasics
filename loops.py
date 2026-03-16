b08c5cbe98e03bc62d113e6a848bed8e0c2f69fc
SrilakshmiSeelavalasa <seelavalasasrilakshmi@gmail.com>
2026-03-16 11:08:01 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,107 @@
+#loops
+#for loop
+
+#using list
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=",")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#using tuple
+
+'''a=(24,25,26,27,28,29)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#using sets
+
+'''num={12,14,16,18,20}
+for i in num:
+    print(i)
+    print(type(num))
+    print(type(i))'''
+
+'''fruits={"mango","apple","grapes"}
+for i in fruits:
+    print(i)
+    print(type(fruits))
+    print(type(i))'''
+
+#using dictionary
+
+'''a={"year":2026,"month":3,"date":14}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a={"year":2026,"month":3,"date":14}
+for i in a.values():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a={"year":2026,"month":3,"date":14}
+for i in a.items():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a={"year":2026,"month":3,"date":14}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.values():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.items():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#using string
+
+'''institute="Codegnan"
+for i in institute:
+    print(i,end=" ")
+    print(type(institute))
+    print(type(i))'''
+
+#using list
+#we have to print["CODEGNAN","PYTHON","COURSE"]
+
+'''a=["codegnan","python","course"]
+b=str(a)
+print(b.upper())'''
+
+'''a=["codegnan","python","course"]
+for i in a:
+    print(i.upper())'''
+
+'''a=["codegnan","python","course"]
+for i in a:
+    print(i.upper(),end=" ")'''
+
+
+
+    
+
+
